30babdf6f5494fe8f9a3878792879e06779caea7
Ananya <shivania.palnitkar@gmail.com>
2026-09-24 07:48:13 +0530

    Revert "Normalize cluster centroid to unit vector"
    This reverts commit 8133abde2ff6605d4dc764b3f3d865457675625d.

--- backend/app/pipeline.py
+++ backend/app/pipeline.py
@@ -56,10 +56,7 @@ async def process_message(msg: NormalizedMessage, db: AsyncSession) -> dict:
     old_count = cluster.member_count - 1
     if len(cluster.centroid) != len(vector):
         raise ValueError("Embedding dimension does not match cluster centroid")
-    # Compute running mean and re-normalize centroid to unit vector:
-    new_centroid = np.array([((cluster.centroid[i] * old_count) + vector[i]) / cluster.member_count for i in range(len(vector))], dtype=np.float32)
-    norm = np.linalg.norm(new_centroid)
-    cluster.centroid = (new_centroid / norm if norm > 0 else new_centroid).tolist()
+    cluster.centroid = [((cluster.centroid[i] * old_count) + vector[i]) / cluster.member_count for i in range(len(vector))]
     parents = list((await db.scalars(select(RawMessage).where(RawMessage.cluster_id == cluster.id, RawMessage.timestamp < ts))).all())
     edge = None
     if parents:
